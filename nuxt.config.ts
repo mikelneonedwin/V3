a1bede4c2812c2f69ba7cd7fafc50f5f9098ee4a
Mikel Edwin <113316417+mikelneonedwin@users.noreply.github.com>
2023-10-05 01:24:45 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,8 +14,7 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: '/css/google-font.css' }
       ],
       meta: [
-        { name: 'viewport', content: "width=device-width, initial-scale=1, user-scalable=no" },
-        { name: 'description', content: "Explore limitless melodies with Vocol - Your go-to destination for seamless music streaming. Discover, listen, and enjoy an extensive collection of songs spanning various genres. Elevate your musical experience with Vocol today." }
+        { name: 'viewport', content: "width=device-width, initial-scale=1, user-scalable=no" }
       ]
     }
   },
